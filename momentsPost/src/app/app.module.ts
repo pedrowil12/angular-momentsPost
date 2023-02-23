f3026fa3aa19a5e02aeb187dbc03be156a09d934
Pedro Willian <70727334+pedrowil12@users.noreply.github.com>
2023-02-23 19:34:25 -0300

    Aplicação de estilo por diretivas.

--- momentsPost/src/app/app.module.ts
+++ momentsPost/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppDados1Component } from './components/app-dados1/app-dados1.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppDados1Component
+    AppDados1Component,
+    ParentDataComponent,
+    DirectivesComponent
   ],
   imports: [
     BrowserModule,
